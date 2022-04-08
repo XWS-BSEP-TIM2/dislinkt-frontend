8d2b42c0a9aa0f6893454e7c188c415170bfaf5e
Tara Pogančev <tarapogancev@gmail.com>
2022-04-08 09:30:50 +0200

    feat: sidebar component

--- Frontend/src/app/components/sidebar/sidebar.component.ts
+++ Frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  currentPage?: string = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    console.log(this.router.url);
+    this.currentPage = this.router.url.substring(1);
   }
 
+  redirect() {
+    console.log(this.currentPage);
+    this.router.navigate([this.currentPage]);
+  }
 }
