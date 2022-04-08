dc66d25309bc10977e7b531f0831114400442d28
Tara Pogančev <tarapogancev@gmail.com>
2022-04-08 11:16:46 +0200

    feat: initial feed page

---
+++ Frontend/src/app/components/suggested-job-offers/single-job-offer/single-job-offer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'single-job-offer',
+  templateUrl: './single-job-offer.component.html',
+  styleUrls: ['./single-job-offer.component.scss'],
+})
+export class SingleJobOfferComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  redirect() {}
+}
