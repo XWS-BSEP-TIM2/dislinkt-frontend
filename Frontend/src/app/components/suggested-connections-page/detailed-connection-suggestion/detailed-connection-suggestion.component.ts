ae668c0b9be8f2d1d984258acfdcbb605bbe5760
Tara Pogančev <tarapogancev@gmail.com>
2022-04-11 23:40:38 +0200

    feat: front connections page

---
+++ Frontend/src/app/components/suggested-connections-page/detailed-connection-suggestion/detailed-connection-suggestion.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'detailed-connection-suggestion',
+  templateUrl: './detailed-connection-suggestion.component.html',
+  styleUrls: ['./detailed-connection-suggestion.component.scss'],
+})
+export class DetailedConnectionSuggestionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  redirect() {}
+}
