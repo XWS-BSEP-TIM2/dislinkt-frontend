e7b320a899fe9bc0029d7c5207180757c500e964
Tara Pogančev <tarapogancev@gmail.com>
2022-04-13 09:43:44 +0200

    feat: single post display page with comments

--- Frontend/src/app/components/post-display/post-display.component.ts
+++ Frontend/src/app/components/post-display/post-display.component.ts
@@ -3,20 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'post-display',
   templateUrl: './post-display.component.html',
-  styleUrls: ['./post-display.component.scss']
+  styleUrls: ['./post-display.component.scss'],
 })
 export class PostDisplayComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  redirectAuthorProfile(){
-
+  redirectAuthorProfile() {
+    window.location.href = 'user/1';
   }
 
   redirectPostPage() {
-    
+    window.location.href = 'post/1';
   }
 }
