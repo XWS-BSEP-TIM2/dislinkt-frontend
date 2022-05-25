e3c2e33148c217f5618caa845cc14ed1b8130378
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2022-05-25 14:35:51 +0200

    feat: password validation, and strength

--- Frontend/src/app/model/registrationModel.ts
+++ Frontend/src/app/model/registrationModel.ts
@@ -16,6 +16,23 @@ export class RegistrationModel {
         
     ) { }
 
+    passwordStrength(): [string, boolean] {
+
+        if (this.password === '') return ["", false];
+
+        if ((this.username !== '' && this.password.toLowerCase().includes(this.username.toLowerCase()))
+            || (this.name !== '' && this.password.toLowerCase().includes(this.name.toLowerCase()))
+            || (this.surname !== '' && this.password.toLowerCase().includes(this.surname.toLowerCase())) ) {
+            return ["Password should not contain you Username or Name", false]
+        }
+
+        var decimal=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,40}$/;
+        if (this.password.match(decimal)) {
+            return ["Password is strong", true];
+        }
+        return ["Password is week (password must contain at least 8 charters with lower, upper, digit and special character)", false];
+    }
+
     passwordMatch(): boolean {
         return this.password === this.confirmPassword;
     }
@@ -26,7 +43,8 @@ export class RegistrationModel {
                 return false;
             }
         }
-        return this.passwordMatch();
+        let [t, b] = this.passwordStrength();
+        return this.passwordMatch() && b;
     }
 
 }
