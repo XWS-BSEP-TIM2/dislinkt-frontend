a45eb9c9049e83cf39ec544ca7e749785c28101d
Tara Pogančev <tarapogancev@gmail.com>
2022-06-02 22:06:58 +0200

    fix: connections page

--- Frontend/src/app/components/profile-page/profile-connections/profile-connections.component.ts
+++ Frontend/src/app/components/profile-page/profile-connections/profile-connections.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ConnectionDetails } from 'src/app/model/connectionDetails';
 import { ConnectionService } from 'src/app/services/connection.service';
 import { LoginService } from 'src/app/services/login.service';
@@ -9,21 +10,22 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./profile-connections.component.scss'],
 })
 export class ProfileConnectionsComponent implements OnInit {
+  friends: ConnectionDetails[] = [];
+
   constructor(
-    private loginService: LoginService,
+    private route: ActivatedRoute,
     private connectionService: ConnectionService
   ) {}
 
-  friends!: ConnectionDetails[];
-
-  loadData() {
-    let targetUserID = window.location.pathname.substring(6).split('/')[0];
-    this.connectionService.GetFriends(targetUserID).subscribe((data) => {
-      this.friends = data;
-    });
-  }
-
   ngOnInit(): void {
     this.loadData();
+  }
+
+  loadData() {
+    let targetUserID = window.location.pathname.substring(6).split('/')[1];
+    this.connectionService.GetFriends(targetUserID).subscribe((data) => {
+      console.log(data);
+      this.friends = data;
+    });
   }
 }
